c5bb39e1c6ddbaa6639ea12c1c5259462e5ce115
osmium <osmium@osmium.dev>
2022-10-31 13:15:57 -0400

    Oct 31

--- js/changLink.js
+++ js/changLink.js
@@ -3,13 +3,19 @@ $(document).ready(function(){
     $('a').each(function(){
 
         $(this).wrap("<strike>");
+        /*
+        $(this).css("text-decoration", "line-through");
+        */
         $(this).attr("href", "http://www.example.com/"); 
 
     });
 
+    var imgs = ["img/usca1.jpg", "img/usca2.jpg", "img/usca3.jpg"];
+    var i = 1;
     $('img').each(function(){
 
-        $(this).attr("src", "img/usca1.jpg");
+        $(this).attr("src", imgs[(i % 3)]);
+        i++;
 
     })
 
